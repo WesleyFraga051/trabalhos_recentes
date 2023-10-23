47a63ce0633d6d4d5c4cf9ec5ae3d2a0c57b9c62
WesleyFraga051 <143102034+WesleyFraga051@users.noreply.github.com>
2023-10-23 16:41:42 -0300

    Add files via upload

---
+++ Consulta Vencimento/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Email
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "  F3     Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 62);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "  F4    Executar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1294, 504);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Dias Em atraso";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1319, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Envia email";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Consulta Vencimento/Form1.cs
@@ -0,0 +1,218 @@
+﻿///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////Systema dessenvolvido Por Wesley fraga\\\\\\\\\\\\\\\\
+///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Server;
+using Oracle.ManagedDataAccess.Client;
+using System.IO;
+using System.Text;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Collections.Generic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using System.Net;
+using System.Net.Mail;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+
+namespace Email
+{
+    public partial class Form1 : Form
+    {
+        string conStr = "User Id=rmsprod;Password=rmsprod;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.1.1.14)(PORT=1521)))(CONNECT_DATA=(SID=sandbox)))";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true; // Habilitar a captura de teclas no formulário
+            this.KeyDown += Form1_KeyDown; // Associar o evento KeyDown ao manipulador de evento
+            comboBox1.Items.Add("5");
+            comboBox1.Items.Add("10");
+            comboBox1.Items.Add("15");
+        }
+
+        public void Variavel(out string Dias)
+        {
+            Dias = comboBox1.SelectedItem.ToString();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F4)
+            {
+                button1_Click(sender, e); // Chamar o evento do botão executar
+            }
+            else if (e.KeyCode == Keys.F3)
+            {
+                button2_Click(sender, e);
+            }
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();// Fechar o Formulario
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            consultaemail();
+            Consulta();
+        }
+
+        private void Email(string destino, string conteudo)
+        {
+
+            //cria uma mensagem
+            MailMessage mail = new MailMessage();
+
+            //define os endereços
+            mail.From = new MailAddress("analista.dev@centermastersul.com.br", "Wesley Fraga");
+            //destino
+            mail.To.Add(destino);
+
+            //define o conteúdo(assunto)
+            mail.Subject = "Este é um simples ,muito simples email";
+            //texto do email
+            mail.Body = conteudo;
+            mail.IsBodyHtml = true;
+
+
+            //envia a mensagem
+            SmtpClient smtp = new SmtpClient("zimbra.centermastersul.com.br");
+            smtp.Send(mail);
+        }
+
+
+        private void Consulta()
+        {
+            string Dias;
+            Variavel(out Dias);
+            //string filial = fil.ToString();
+            string sqlQuery = "select capap_compr as comprador," +
+                 " CAPAP_NUMERO as Numero_acordo," +
+                 " CAPAP_FORNEC as fornecedor," +
+                 " SUBSTR(TO_CHAR(D.DUP_VENC,'fm0000000'),6,2)||'/'||SUBSTR(TO_CHAR(D.DUP_VENC,'fm0000000'),4,2)||'/'||SUBSTR(TO_CHAR(D.DUP_VENC,'fm0000000'),2,2) as vencimento," +
+                 " D.DUP_VALOR AS VALOR_DUPLICATA," +
+                 //" ((SELECT SUBSTR(TO_CHAR(SYSDATE, 'YY'), 3, 1) || '1' || SUBSTR(TO_CHAR(SYSDATE, 'YYMMDD'), 1)FROM DUAL) -D.DUP_VENC) *0.33 / 100  AS JUROS," +
+                // " round(D.DUP_VALOR * ((SELECT SUBSTR(TO_CHAR(SYSDATE, 'YY'), 3, 1) || '1' || SUBSTR(TO_CHAR(SYSDATE, 'YYMMDD'), 1)FROM DUAL) -D.DUP_VENC) *0.33 / 100 + D.DUP_VALOR,2) AS VALOR_COM_JUROS," +
+                 " CAPAP_AGENDA AS AGENDA," +
+                 " D.DUP_VENC - (SELECT SUBSTR(TO_CHAR(SYSDATE, 'YY'), 3, 1) || '1' || SUBSTR(TO_CHAR(SYSDATE, 'YYMMDD'), 1)FROM DUAL)  as dias_a_vencer" +
+                 " FROM AG1CAPAP, AA1RTITU d " +
+                 " where" +
+                 " CAPAP_NUMERO = DUP_TITULO_ALT" +
+                 " and CAPAP_AGENDA = DUP_AGENDA" +
+            " AND CAPAP_FLAG_ATU          in(1)" +
+                 " AND DUP_VENC  between(SELECT SUBSTR(TO_CHAR(SYSDATE, 'YY'), 3, 1) || '1' || SUBSTR(TO_CHAR(SYSDATE , 'YYMMDD'), 1)FROM DUAL)" +
+                 " and(SELECT SUBSTR(TO_CHAR(SYSDATE, 'YY'), 3, 1) || '1' || SUBSTR(TO_CHAR(SYSDATE + "+ Dias + ", 'YYMMDD'), 1)FROM DUAL)" +
+                 " AND DUP_DT_PAG = 0" +
+                 " ORDER BY DIAS_VENCIDOS DESC ";
+
+            using (OracleConnection con = new OracleConnection(conStr))
+            {
+                con.Open();
+                using (OracleCommand command = new OracleCommand(sqlQuery, con))
+                {
+                    using (OracleDataAdapter adapter = new OracleDataAdapter(command))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt); // Preencha o DataTable com os resultados da consulta
+
+                        StringBuilder conteudoEmail = new StringBuilder();
+                        conteudoEmail.AppendLine("<html><body><table>");
+
+                        // Adicione a linha de cabeçalho da tabela
+                        conteudoEmail.AppendLine("<tr>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Comprador</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Número do Acordo</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Fornecedor</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Vencimento</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Valor da Duplicata</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Valor com Juros</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Agenda</th>");
+                        conteudoEmail.AppendLine($"<th style=\"padding: 10px;\">Dias Atrasado</th>");
+                        conteudoEmail.AppendLine("</tr>");
+
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            string conteudo1 = dt.Rows[i]["comprador"].ToString();
+                            string conteudo2 = dt.Rows[i]["Numero_acordo"].ToString();
+                            string conteudo3 = dt.Rows[i]["fornecedor"].ToString();
+                            string conteudo4 = dt.Rows[i]["vencimento"].ToString();
+                            string conteudo7 = dt.Rows[i]["VALOR_DUPLICATA"].ToString();
+                            string conteudo8 = dt.Rows[i]["VALOR_COM_JUROS"].ToString();
+                            string conteudo9 = dt.Rows[i]["AGENDA"].ToString();
+                            string conteudox = dt.Rows[i]["dias_vencidos"].ToString() ;
+
+                            // Adicione cada linha de dados à tabela com alinhamento central
+                            conteudoEmail.AppendLine("<tr>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo1}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo2}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo3}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo4}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo7}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo8}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudo9}</td>");
+                            conteudoEmail.AppendLine($"<td align=\"center\">{conteudox}</td>");
+                            conteudoEmail.AppendLine("</tr>");
+                        }
+
+                        conteudoEmail.AppendLine("</table></body></html>");
+
+                        // Agora você pode enviar o email com as informações em formato HTML
+                        string destino = consultaemail(); // Chame consultaemail para obter o destino
+                        Email(destino, conteudoEmail.ToString());
+
+                        dataGridView1.DataSource = dt; // Configure o DataGridView para exibir os dados
+
+
+                    }
+                }
+            }
+        }
+
+
+        private string consultaemail()
+        {
+            string query = "SELECT * FROM AA1RMSUS WHERE LTRIM(RTRIM(USU_NOME)) LIKE '%TI03%'";
+
+            using (OracleConnection con = new OracleConnection(conStr))
+            {
+                con.Open();
+                using (OracleCommand command = new OracleCommand(query, con))
+                {
+                    using (OracleDataAdapter adapter = new OracleDataAdapter(command))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        StringBuilder destinoEmail = new StringBuilder();
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            string email = row["USU_EMAIL"].ToString();
+                            destinoEmail.AppendLine(email);
+                        }
+
+                        // Retorna o destino como uma string
+                        return destinoEmail.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
+
