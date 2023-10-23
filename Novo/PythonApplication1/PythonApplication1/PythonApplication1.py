a69d8adc768d707ad388e86de5e75c77e0014237
WesleyFraga051 <143102034+WesleyFraga051@users.noreply.github.com>
2023-10-23 16:39:20 -0300

    Add files via upload

---
+++ Novo/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,25 @@
+
+#Lista é uma coleção ordenada e mutável. Permite Membros duplicados
+lista =["carro",True,2, 3.5]
+print(lista)
+print(type(lista))
+print("-"*30)
+
+#tupla é uma coleção imutavel
+tupla =("carro",True,2 ,3.5)
+print(tupla)
+print(type(tupla))
+print("-"*30)
+
+#dicionario ordenada mutavel .nenhum membro repete
+#chave:valor
+dicionario = {"nome":"carro","logica": True, "numero":2,"outroNumero":3.5}
+print(dicionario)
+print(type(dicionario))
+print("-"*30)
+
+#set é uma coleção não ordenada e não indexada. nenhum membro duplicado.
+conjunto ={"carro",True,2,3.5}
+print(conjunto)
+print(type(conjunto))
+print("-"*30)
