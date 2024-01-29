e377b89b53ecbdaec68926ddbc6c30aedd1cfdc4
Wesley Fraga <143102034+WesleyFraga051@users.noreply.github.com>
2024-01-29 16:45:12 -0300

    att net framework 6.0

---
+++ Pedidos incluidos e Alterados/attframework/Pedincal/Pedincal/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Pedincal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            textBox2 = new TextBox();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            label6 = new Label();
+            comboBox2 = new ComboBox();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label1 = new Label();
+            toolTip1 = new ToolTip(components);
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(21, 194);
+            dataGridView1.Margin = new Padding(4, 3, 4, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1510, 582);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(97, 14);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(66, 48);
+            button1.TabIndex = 1;
+            button1.Text = "  F4    Executar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(21, 14);
+            button2.Margin = new Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(69, 48);
+            button2.TabIndex = 2;
+            button2.Text = "     F3       Sair";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.ImeMode = ImeMode.On;
+            dateTimePicker1.Location = new Point(7, 39);
+            dateTimePicker1.Margin = new Padding(4, 3, 4, 3);
+            dateTimePicker1.MaxDate = new DateTime(3000, 12, 31, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(94, 23);
+            dateTimePicker1.TabIndex = 3;
+            dateTimePicker1.Value = new DateTime(2023, 8, 22, 0, 0, 0, 0);
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.ImeMode = ImeMode.On;
+            dateTimePicker2.Location = new Point(158, 39);
+            dateTimePicker2.Margin = new Padding(4, 3, 4, 3);
+            dateTimePicker2.MaxDate = new DateTime(3000, 12, 31, 0, 0, 0, 0);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(94, 23);
+            dateTimePicker2.TabIndex = 4;
+            dateTimePicker2.Value = new DateTime(2023, 8, 22, 0, 0, 0, 0);
+            dateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.LightSkyBlue;
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.FlatStyle = FlatStyle.Popup;
+            groupBox1.ForeColor = SystemColors.Desktop;
+            groupBox1.Location = new Point(21, 70);
+            groupBox1.Margin = new Padding(5, 5, 5, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 3, 4, 3);
+            groupBox1.RightToLeft = RightToLeft.No;
+            groupBox1.Size = new Size(455, 115);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Filtros Para Pedidos alterados";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(300, 66);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Usuário";
+            toolTip1.SetToolTip(label8, "Codigo do fornecedor Sem digito\r\n");
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(303, 82);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(97, 23);
+            textBox2.TabIndex = 17;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(300, 21);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(126, 15);
+            label7.TabIndex = 16;
+            label7.Text = "Codigo do Fornecedor";
+            toolTip1.SetToolTip(label7, "Codigo do fornecedor Sem digito\r\n");
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(303, 39);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(97, 23);
+            textBox1.TabIndex = 15;
+            toolTip1.SetToolTip(textBox1, "Fornecedor sem digito");
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(154, 66);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(31, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Filial";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(158, 82);
+            comboBox2.Margin = new Padding(4, 3, 4, 3);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(94, 23);
+            comboBox2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(7, 66);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Situação";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(7, 84);
+            comboBox1.Margin = new Padding(4, 3, 4, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(94, 23);
+            comboBox1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(154, 18);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(137, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Data Movimentação Fim";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(4, 18);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(146, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Data Movimentação Inicio";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(170, 14);
+            button3.Margin = new Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.RightToLeft = RightToLeft.No;
+            button3.Size = new Size(66, 48);
+            button3.TabIndex = 6;
+            button3.Text = "  F6    Texto";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(317, 14);
+            button4.Margin = new Padding(4, 3, 4, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(66, 48);
+            button4.TabIndex = 7;
+            button4.Text = " F10    Excel";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(244, 14);
+            button5.Margin = new Padding(4, 3, 4, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(66, 48);
+            button5.TabIndex = 8;
+            button5.Text = " F9  + Filtros";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSkyBlue;
+            ClientSize = new Size(1329, 576);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(groupBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Pedidos Alterados";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        protected System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pedidos incluidos e Alterados/attframework/Pedincal/Pedincal/Form2.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Pedincal
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            Btsair = new Button();
+            Btexecutar = new Button();
+            Bttxt = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            textBox2 = new TextBox();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            label6 = new Label();
+            comboBox2 = new ComboBox();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            dateTimePicker4 = new DateTimePicker();
+            dateTimePicker3 = new DateTimePicker();
+            label4 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Btsair
+            // 
+            Btsair.Location = new Point(24, 14);
+            Btsair.Margin = new Padding(4, 3, 4, 3);
+            Btsair.Name = "Btsair";
+            Btsair.Size = new Size(69, 48);
+            Btsair.TabIndex = 3;
+            Btsair.Text = "     F3       Sair";
+            Btsair.UseVisualStyleBackColor = true;
+            Btsair.Click += Btsair_Click;
+            // 
+            // Btexecutar
+            // 
+            Btexecutar.Location = new Point(100, 14);
+            Btexecutar.Margin = new Padding(4, 3, 4, 3);
+            Btexecutar.Name = "Btexecutar";
+            Btexecutar.Size = new Size(66, 48);
+            Btexecutar.TabIndex = 4;
+            Btexecutar.Text = "  F4    Executar";
+            Btexecutar.UseVisualStyleBackColor = true;
+            Btexecutar.Click += Btexecutar_Click;
+            // 
+            // Bttxt
+            // 
+            Bttxt.Location = new Point(174, 14);
+            Bttxt.Margin = new Padding(4, 3, 4, 3);
+            Bttxt.Name = "Bttxt";
+            Bttxt.RightToLeft = RightToLeft.No;
+            Bttxt.Size = new Size(66, 48);
+            Bttxt.TabIndex = 7;
+            Bttxt.Text = "  F6    Texto";
+            Bttxt.UseVisualStyleBackColor = true;
+            Bttxt.Click += Bttxt_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(247, 14);
+            button5.Margin = new Padding(4, 3, 4, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(66, 48);
+            button5.TabIndex = 9;
+            button5.Text = " F9  + Filtros";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click_1;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(321, 14);
+            button4.Margin = new Padding(4, 3, 4, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(66, 48);
+            button4.TabIndex = 10;
+            button4.Text = " F10    Excel";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click_1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(24, 194);
+            dataGridView1.Margin = new Padding(4, 3, 4, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1506, 527);
+            dataGridView1.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.LightSkyBlue;
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(dateTimePicker4);
+            groupBox1.Controls.Add(dateTimePicker3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.FlatStyle = FlatStyle.Popup;
+            groupBox1.ForeColor = SystemColors.Desktop;
+            groupBox1.Location = new Point(24, 70);
+            groupBox1.Margin = new Padding(5, 5, 5, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 3, 4, 3);
+            groupBox1.RightToLeft = RightToLeft.No;
+            groupBox1.Size = new Size(450, 115);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Filtros Para Pedidos alterados";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(287, 65);
+            label8.Margin = new Padding(4, 0, 4, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(47, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Usuário";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(290, 81);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(107, 23);
+            textBox2.TabIndex = 17;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(287, 18);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(126, 15);
+            label7.TabIndex = 16;
+            label7.Text = "Codigo do Fornecedor";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(290, 36);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(107, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(146, 65);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(31, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Filial";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(149, 82);
+            comboBox2.Margin = new Padding(4, 3, 4, 3);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(94, 23);
+            comboBox2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 65);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Situação";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(10, 82);
+            comboBox1.Margin = new Padding(4, 3, 4, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(94, 23);
+            comboBox1.TabIndex = 11;
+            // 
+            // dateTimePicker4
+            // 
+            dateTimePicker4.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker4.Format = DateTimePickerFormat.Custom;
+            dateTimePicker4.ImeMode = ImeMode.On;
+            dateTimePicker4.Location = new Point(149, 37);
+            dateTimePicker4.Margin = new Padding(4, 3, 4, 3);
+            dateTimePicker4.MaxDate = new DateTime(3000, 12, 31, 0, 0, 0, 0);
+            dateTimePicker4.Name = "dateTimePicker4";
+            dateTimePicker4.Size = new Size(94, 23);
+            dateTimePicker4.TabIndex = 10;
+            dateTimePicker4.Value = new DateTime(2023, 8, 22, 0, 0, 0, 0);
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            dateTimePicker3.ImeMode = ImeMode.On;
+            dateTimePicker3.Location = new Point(10, 36);
+            dateTimePicker3.Margin = new Padding(4, 3, 4, 3);
+            dateTimePicker3.MaxDate = new DateTime(3000, 12, 31, 0, 0, 0, 0);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(94, 23);
+            dateTimePicker3.TabIndex = 9;
+            dateTimePicker3.Value = new DateTime(2023, 8, 22, 0, 0, 0, 0);
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(146, 18);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(127, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Data Recebimento Fim";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(7, 18);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Data Recebimento Inicio";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSkyBlue;
+            ClientSize = new Size(1362, 673);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(Bttxt);
+            Controls.Add(Btexecutar);
+            Controls.Add(Btsair);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form2";
+            Text = "Pedidos incluidos e alterados por Data de recebimento";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btsair;
+        private System.Windows.Forms.Button Btexecutar;
+        private System.Windows.Forms.Button Bttxt;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        protected System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
